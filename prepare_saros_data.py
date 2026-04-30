114fbdec307eea0189d298d337cdcf52ca7323b2
Andreas With Aspe <andreasaspe@gmail.com>
2026-04-30 11:39:13 +0200

    Clean and parallelized

--- prepare_saros_data.py
+++ prepare_saros_data.py
@@ -14,218 +14,215 @@ DEFAULT_SRC = "/storage/awias/saros_raw"
 DEFAULT_DST = "/data/awias/periseg/saros/NIFTI_collected_test"
 
 
-def collect_saros_cases(src_path, dst_path, margin_mm=20):
+# ----------------------------
+# SUBJECT PROCESSING (PARALLEL)
+# ----------------------------
+def _process_subject(subject, src_path, dst_path, margin_mm):
+    subject_folder = os.path.join(src_path, subject)
+    subject_id = subject.split("_")[1]
+
+    try:
+        img_sitk = sitk.ReadImage(os.path.join(subject_folder, "image.nii.gz"))
+        trunk_sitk = sitk.ReadImage(os.path.join(subject_folder, f"{subject}_trunkcavities.nii.gz"))
+        highres_sitk = sitk.ReadImage(os.path.join(subject_folder, f"{subject}_heartchambershighres.nii.gz"))
+        total_sitk = sitk.ReadImage(os.path.join(subject_folder, f"{subject}_total.nii.gz"))
+        coronary_sitk = sitk.ReadImage(os.path.join(subject_folder, f"{subject}_coronaryarteries.nii.gz"))
+        bodyregions_sitk = sitk.ReadImage(os.path.join(subject_folder, "body-regions.nii.gz"))
+    except Exception:
+        return None
+
+    # Align metadata
+    for img in [trunk_sitk, highres_sitk, total_sitk, coronary_sitk]:
+        img.CopyInformation(bodyregions_sitk)
+
+    img_arr = sitk.GetArrayFromImage(img_sitk)
+    mask = img_arr != -1024
+
+    coords = np.where(mask)
+    z_min, z_max = coords[0].min(), coords[0].max()
+    y_min, y_max = coords[1].min(), coords[1].max()
+    x_min, x_max = coords[2].min(), coords[2].max()
+
+    start = [int(x_min), int(y_min), int(z_min)]
+    size = [
+        int(x_max - x_min + 1),
+        int(y_max - y_min + 1),
+        int(z_max - z_min + 1),
+    ]
+
+    def crop(img):
+        return sitk.RegionOfInterest(img, size=size, index=start)
+
+    img_sitk = crop(img_sitk)
+    trunk_sitk = crop(trunk_sitk)
+    highres_sitk = crop(highres_sitk)
+    total_sitk = crop(total_sitk)
+    coronary_sitk = crop(coronary_sitk)
+    bodyregions_sitk = crop(bodyregions_sitk)
+
+    trunk = (sitk.GetArrayFromImage(trunk_sitk) == 3).astype(np.uint8)
+    highres = (sitk.GetArrayFromImage(highres_sitk) == 2).astype(np.uint8)
+    bodyregions = (sitk.GetArrayFromImage(bodyregions_sitk) == 7).astype(np.uint8)
+
+    if bodyregions.sum() == 0 or trunk.sum() == 0 or highres.sum() == 0:
+        return None
+
+    # Remove border-touching cases
+    if (
+        trunk[0].sum() > 0 or trunk[-1].sum() > 0 or
+        trunk[:, 0].sum() > 0 or trunk[:, -1].sum() > 0 or
+        trunk[:, :, 0].sum() > 0 or trunk[:, :, -1].sum() > 0
+    ):
+        return None
+
+    if (
+        highres[0].sum() > 0 or highres[-1].sum() > 0 or
+        highres[:, 0].sum() > 0 or highres[:, -1].sum() > 0 or
+        highres[:, :, 0].sum() > 0 or highres[:, :, -1].sum() > 0
+    ):
+        return None
+
+    spacing = trunk_sitk.GetSpacing()
+    size_img = img_sitk.GetSize()
+
+    nz = np.where(trunk > 0)
+    z_min, z_max = nz[0].min(), nz[0].max()
+    y_min, y_max = nz[1].min(), nz[1].max()
+    x_min, x_max = nz[2].min(), nz[2].max()
+
+    margin_vox = [int(margin_mm / s) for s in spacing]
+
+    x_start = max(0, x_min - margin_vox[0])
+    x_end = min(size_img[0], x_max + margin_vox[0] + 1)
+    y_start = max(0, y_min - margin_vox[1])
+    y_end = min(size_img[1], y_max + margin_vox[1] + 1)
+    z_start = max(0, z_min - margin_vox[2])
+    z_end = min(size_img[2], z_max + margin_vox[2] + 1)
+
+    roi_size = [x_end - x_start, y_end - y_start, z_end - z_start]
+
+    def roi(img):
+        return sitk.RegionOfInterest(
+            img,
+            size=roi_size,
+            index=[x_start, y_start, z_start],
+        )
+
+    return {
+        "id": subject_id,
+        "img": roi(img_sitk),
+        "trunk": roi(trunk_sitk),
+        "highres": roi(highres_sitk),
+        "total": roi(total_sitk),
+        "coronary": roi(coronary_sitk),
+        "label": roi(bodyregions_sitk),
+    }
+
+
+# ----------------------------
+# MAIN COLLECTION FUNCTION
+# ----------------------------
+def collect_saros_cases_parallel(src_path, dst_path, margin_mm=20, workers=None):
     os.makedirs(dst_path, exist_ok=True)
 
-    all_subjects = [x for x in os.listdir(src_path) if x.startswith("case")]
+    subjects = [x for x in os.listdir(src_path) if x.startswith("case")]
 
-    for subject in all_subjects:
-        subject_id = subject.split("_")[1]
-        print(subject)
-        subject_folder = os.path.join(src_path, subject)
+    if workers is None:
+        workers = max(os.cpu_count() - 1, 1)
 
-        src_img_path = os.path.join(subject_folder, "image.nii.gz")
-        src_ts_highres_path = os.path.join(subject_folder, f"{subject}_heartchambershighres.nii.gz")
-        src_ts_total_path = os.path.join(subject_folder, f"{subject}_total.nii.gz")
-        src_ts_coronary_path = os.path.join(subject_folder, f"{subject}_coronaryarteries.nii.gz")
-        src_ts_trunk_path = os.path.join(subject_folder, f"{subject}_trunkcavities.nii.gz")
-        src_bodyregions_path = os.path.join(subject_folder, "body-regions.nii.gz")
+    worker_fn = partial(
+        _process_subject,
+        src_path=src_path,
+        dst_path=dst_path,
+        margin_mm=margin_mm,
+    )
 
-        try:
-            img_sitk = sitk.ReadImage(src_img_path)
-            trunk_sitk = sitk.ReadImage(src_ts_trunk_path)
-            highres_sitk = sitk.ReadImage(src_ts_highres_path)
-            total_sitk = sitk.ReadImage(src_ts_total_path)
-            coronary_sitk = sitk.ReadImage(src_ts_coronary_path)
-            bodyregions_sitk = sitk.ReadImage(src_bodyregions_path)
-        except Exception as exc:
-            print(f"ERROR reading files for {subject}: {exc}")
-            continue
-
-        # Normalize metadata to match body-regions
-        img_sitk.CopyInformation(bodyregions_sitk)
-        trunk_sitk.CopyInformation(bodyregions_sitk)
-        highres_sitk.CopyInformation(bodyregions_sitk)
-        total_sitk.CopyInformation(bodyregions_sitk)
-        coronary_sitk.CopyInformation(bodyregions_sitk)
-
-        img_arr = sitk.GetArrayFromImage(img_sitk)  # z, y, x
-
-        mask = img_arr != -1024
-        coords = np.where(mask)
-        z_min, z_max = coords[0].min(), coords[0].max()
-        y_min, y_max = coords[1].min(), coords[1].max()
-        x_min, x_max = coords[2].min(), coords[2].max()
-
-        start = [int(x_min), int(y_min), int(z_min)]
-        size = [
-            int(x_max - x_min + 1),
-            int(y_max - y_min + 1),
-            int(z_max - z_min + 1),
-        ]
-
-        def crop(img):
-            return sitk.RegionOfInterest(img, size=size, index=start)
-
-        img_sitk = crop(img_sitk)
-        trunk_sitk = crop(trunk_sitk)
-        highres_sitk = crop(highres_sitk)
-        total_sitk = crop(total_sitk)
-        coronary_sitk = crop(coronary_sitk)
-        bodyregions_sitk = crop(bodyregions_sitk)
-
-        trunk = sitk.GetArrayFromImage(trunk_sitk)
-        trunk = (trunk == 3).astype(int)
-
-        highres = sitk.GetArrayFromImage(highres_sitk)
-        highres = (highres == 2).astype(int)
-
-        bodyregions = sitk.GetArrayFromImage(bodyregions_sitk)
-        bodyregions = (bodyregions == 7).astype(int)
-
-        if bodyregions.sum() == 0:
-            continue
-        if trunk.sum() == 0:
-            continue
-        if highres.sum() == 0:
-            continue
-
-        if (
-            trunk[0, :, :].sum() > 0
-            or trunk[:, 0, :].sum() > 0
-            or trunk[:, -1, :].sum() > 0
-            or trunk[:, :, 0].sum() > 0
-            or trunk[:, :, -1].sum() > 0
-        ):
-            continue
-
-        if (
-            highres[0, :, :].sum() > 0
-            or highres[-1, :, :].sum() > 0
-            or highres[:, 0, :].sum() > 0
-            or highres[:, -1, :].sum() > 0
-            or highres[:, :, 0].sum() > 0
-            or highres[:, :, -1].sum() > 0
-        ):
-            continue
-
-        print(f"SURVIVED ALL CHECKS AND WILL BE INCLUDED: {subject}")
-
-        spacing = trunk_sitk.GetSpacing()
-        size = img_sitk.GetSize()
-
-        nz = np.where(trunk > 0)
-        z_min, z_max = nz[0].min(), nz[0].max()
-        y_min, y_max = nz[1].min(), nz[1].max()
-        x_min, x_max = nz[2].min(), nz[2].max()
-
-        crop_mm = [margin_mm, margin_mm, margin_mm]
-        margin_vox = [int(crop_mm[i] / spacing[i]) for i in range(3)]
-
-        x_start = max(0, x_min - margin_vox[0])
-        x_end = min(size[0], x_max + margin_vox[0] + 1)
-        y_start = max(0, y_min - margin_vox[1])
-        y_end = min(size[1], y_max + margin_vox[1] + 1)
-        z_start = max(0, z_min - margin_vox[2])
-        z_end = min(size[2], z_max + margin_vox[2] + 1)
-        size_x = x_end - x_start
-        size_y = y_end - y_start
-        size_z = z_end - z_start
-
-        cropped_img_sitk = sitk.RegionOfInterest(
-            img_sitk,
-            size=[int(size_x), int(size_y), int(size_z)],
-            index=[int(x_start), int(y_start), int(z_start)],
-        )
-        cropped_trunk_sitk = sitk.RegionOfInterest(
-            trunk_sitk,
-            size=[int(size_x), int(size_y), int(size_z)],
-            index=[int(x_start), int(y_start), int(z_start)],
-        )
-        cropped_highres_sitk = sitk.RegionOfInterest(
-            highres_sitk,
-            size=[int(size_x), int(size_y), int(size_z)],
-            index=[int(x_start), int(y_start), int(z_start)],
-        )
-        cropped_total_sitk = sitk.RegionOfInterest(
-            total_sitk,
-            size=[int(size_x), int(size_y), int(size_z)],
-            index=[int(x_start), int(y_start), int(z_start)],
-        )
-        cropped_coronary_sitk = sitk.RegionOfInterest(
-            coronary_sitk,
-            size=[int(size_x), int(size_y), int(size_z)],
-            index=[int(x_start), int(y_start), int(z_start)],
-        )
-        cropped_bodyregions_sitk = sitk.RegionOfInterest(
-            bodyregions_sitk,
-            size=[int(size_x), int(size_y), int(size_z)],
-            index=[int(x_start), int(y_start), int(z_start)],
+    with ProcessPoolExecutor(max_workers=workers) as ex:
+        results = list(
+            tqdm(
+                ex.map(worker_fn, subjects),
+                total=len(subjects),
+                desc="Collecting SAROS cases",
+            )
         )
 
-        dst_img_path = os.path.join(dst_path, f"saros_{subject_id}_img.nii.gz")
-        dst_ts_highres_path = os.path.join(dst_path, f"saros_{subject_id}_ts_heartchambershighres.nii.gz")
-        dst_ts_total_path = os.path.join(dst_path, f"saros_{subject_id}_ts_total.nii.gz")
-        dst_ts_coronary_path = os.path.join(dst_path, f"saros_{subject_id}_ts_coronaryarteries.nii.gz")
-        dst_ts_trunk_path = os.path.join(dst_path, f"saros_{subject_id}_ts_trunkcavities.nii.gz")
-        dst_bodyregions_path = os.path.join(dst_path, f"saros_{subject_id}_label.nii.gz")
+    for res in results:
+        if res is None:
+            continue
 
-        sitk.WriteImage(cropped_img_sitk, dst_img_path)
-        sitk.WriteImage(cropped_highres_sitk, dst_ts_highres_path)
-        sitk.WriteImage(cropped_total_sitk, dst_ts_total_path)
-        sitk.WriteImage(cropped_coronary_sitk, dst_ts_coronary_path)
-        sitk.WriteImage(cropped_trunk_sitk, dst_ts_trunk_path)
-        sitk.WriteImage(cropped_bodyregions_sitk, dst_bodyregions_path)
+        sid = res["id"]
+
+        sitk.WriteImage(res["img"], os.path.join(dst_path, f"saros_{sid}_img.nii.gz"))
+        sitk.WriteImage(res["highres"], os.path.join(dst_path, f"saros_{sid}_ts_heartchambershighres.nii.gz"))
+        sitk.WriteImage(res["total"], os.path.join(dst_path, f"saros_{sid}_ts_total.nii.gz"))
+        sitk.WriteImage(res["coronary"], os.path.join(dst_path, f"saros_{sid}_ts_coronaryarteries.nii.gz"))
+        sitk.WriteImage(res["trunk"], os.path.join(dst_path, f"saros_{sid}_ts_trunkcavities.nii.gz"))
+        sitk.WriteImage(res["label"], os.path.join(dst_path, f"saros_{sid}_label.nii.gz"))
 
 
+# ----------------------------
+# REORIENT
+# ----------------------------
 def _reorient_file(file_path, direction):
     if not file_path.endswith(".nii.gz"):
         return None
 
     try:
-        file_sitk = sitk.ReadImage(file_path)
-        file_sitk_reoriented = utils.reorient_sitk(file_sitk, direction)
-        sitk.WriteImage(file_sitk_reoriented, file_path)
-        return os.path.basename(file_path)
-    except Exception as exc:
-        return f"ERROR {os.path.basename(file_path)}: {exc}"
+        img = sitk.ReadImage(file_path)
+        img = utils.reorient_sitk(img, direction)
+        sitk.WriteImage(img, file_path)
+        return True
+    except Exception:
+        return False
 
 
 def reorient_folder(path, direction="LPS", workers=None):
     if workers is None:
         workers = max(os.cpu_count() - 1, 1)
 
-    all_files = [os.path.join(path, f) for f in os.listdir(path)]
+    files = [os.path.join(path, f) for f in os.listdir(path)]
 
     worker = partial(_reorient_file, direction=direction)
 
-    with ProcessPoolExecutor(max_workers=workers) as executor:
-        for _ in tqdm(
-            executor.map(worker, all_files),
-            total=len(all_files),
-            desc="Reorienting files",
-        ):
-            pass
+    with ProcessPoolExecutor(max_workers=workers) as ex:
+        list(
+            tqdm(
+                ex.map(worker, files),
+                total=len(files),
+                desc="Reorienting files",
+            )
+        )
 
 
+# ----------------------------
+# CLI
+# ----------------------------
 def main():
-    parser = argparse.ArgumentParser(description="Prepare SAROS dataset for mesh refinement.")
-    parser.add_argument("--src", default=DEFAULT_SRC, help="Source SAROS dataset folder")
-    parser.add_argument("--dst", default=DEFAULT_DST, help="Output folder for collected NIfTI files")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--src", default=DEFAULT_SRC)
+    parser.add_argument("--dst", default=DEFAULT_DST)
     parser.add_argument("--mode", choices=["organize", "reorient", "both"], default="both")
-    parser.add_argument("--direction", default="LPS", help="Target orientation for reorienting")
-    parser.add_argument("--workers", type=int, default=None, help="Number of workers for reorienting")
-    parser.add_argument("--margin-mm", type=int, default=20, help="Crop margin in mm")
+    parser.add_argument("--direction", default="LPS")
+    parser.add_argument("--workers", type=int, default=None)
+    parser.add_argument("--margin-mm", type=int, default=20)
 
     args = parser.parse_args()
 
     if args.mode in ("organize", "both"):
-        print("Organizing and cropping SAROS cases...")
-        collect_saros_cases(args.src, args.dst, margin_mm=args.margin_mm)
+        collect_saros_cases_parallel(
+            args.src,
+            args.dst,
+            margin_mm=args.margin_mm,
+            workers=args.workers,
+        )
 
     if args.mode in ("reorient", "both"):
-        print("Reorienting files...")
-        reorient_folder(args.dst, direction=args.direction, workers=args.workers)
+        reorient_folder(
+            args.dst,
+            direction=args.direction,
+            workers=args.workers,
+        )
+
 
 if __name__ == "__main__":
     main()
